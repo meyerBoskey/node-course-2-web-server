c90b2deb601195d68f3fbc9e52c681bcee8cc63a
Cullen <cpsulli1991@gmail.com>
2017-09-10 16:44:07 -0500

    Added mindReaderApp

--- server.js
+++ server.js
@@ -53,10 +53,17 @@ app.get('/about', (req, res) => {
   });
 });
 
+
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
     pageTitle: 'Projects Page',
     pageText: 'Portfolio page here'
+  });
+});
+
+app.get('/mind', (req, res) => {
+  res.render('mind.hbs', {
+    pageTitle: 'Mind Reader App',
   });
 });
 
