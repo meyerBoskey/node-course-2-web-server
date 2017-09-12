b6cabe0460fb3894fe0b1a58ca2e4ea5781a298c
Cullen <cpsulli1991@gmail.com>
2017-09-12 16:24:28 -0500

    fixed the table and took out code unrelated to app

---
+++ playground/mindReaderApp.js
@@ -0,0 +1,30 @@
+var readline = require('readline');
+
+/*
+ * We need to create a readline interface.
+ * The interface takes 2 streams.
+ * The input field points to the readable input stream
+ * and the output field to the writable output stream.
+ */
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+  console.log('1. Think of a two digit integer.')
+  console.log('2. Add the two digits of your number together.')
+  console.log('3. Subtract this some from your original number')
+  console.log('4. locate your number below and concentrate on the picture...')
+
+
+rl.question('Please tell me your name: ', function (name) {
+
+    rl.question('Hello ' + name + ' Please pick a number between 1-20: ', function (age) {
+        // process.stdout.write('Well hello ' + answer);
+        // Console.log creates a formatted output from process.stdout.write().
+        console.log(name + ', you are ' + age + ' years old');
+
+        rl.close();
+        process.stdin.destroy();
+    });
+
+});
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.set('view engine', 'hbs');
 //   res.render('maintenance.hbs')
 // });
 
+
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
@@ -33,30 +34,9 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
-
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my super awesome website!',
-    name: 'Cullen',
-    likes: [
-      'lifting',
-      'sports'
-    ]
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page',
-  });
-});
-
-app.get('/projects', (req, res) => {
-  res.render('projects.hbs', {
-    pageTitle: 'Projects Page',
-    pageText: 'Portfolio page here'
+  res.render('mind.hbs', {
+    pageTitle: 'Mind Reader App',
   });
 });
 
